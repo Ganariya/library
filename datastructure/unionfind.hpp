f01b78bc8b59d59fbd350e575aa7601316429f98
ganariya <willwill1997willwill@yahoo.co.jp>
2020-03-04 18:11:51 +0900

    add

--- datastructure/unionfind.hpp
+++ datastructure/unionfind.hpp
@@ -13,7 +13,9 @@ public:
 
     vector<int> par;
     vector<int> sizes;
-
+/**
+     * @brief $get\_connectivity()$: return the number of connectivity
+     */
     UnionFind(int n)
             : par(n), sizes(n, 1) {
         for (int i = 0; i < n; i++) {
@@ -27,7 +29,9 @@ public:
     int find(int x) {
         return x == par[x] ? x : par[x] = find(par[x]);
     }
-
+/**
+     * @brief $get\_connectivity()$: return the number of connectivity
+     */
     bool unite(int x, int y) {
         x = find(x);
         y = find(y);
@@ -37,15 +41,21 @@ public:
         sizes[x] += sizes[y];
         return true;
     }
-
+/**
+     * @brief $get\_connectivity()$: return the number of connectivity
+     */
     bool same(int x, int y) {
         return find(x) == find(y);
     }
-
+/**
+     * @brief $get\_connectivity()$: return the number of connectivity
+     */
     int get_size(int x) {
         return sizes[find(x)];
     }
-
+/**
+     * @brief $get\_connectivity()$: return the number of connectivity
+     */
     bool all_same() {
         bool good = true;
         for (int i = 0, n = par.size(); i < n; i++) if (find(0) != find(i)) good = false;
